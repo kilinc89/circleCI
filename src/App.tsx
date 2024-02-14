6c99316bb3789372dcf8b2a05cd6dcea0b03b2bb
kilinc89 <ibrhmklnc@gmail.com>
2024-02-14 19:51:19 +0300

    a

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React-Staging
+          Learn React-Stagingg
         </a>
       </header>
     </div>
