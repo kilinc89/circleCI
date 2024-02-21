3040b794c6f657ed240d213889eb055fe563ce0f
kilinc89 <ibrhmklnc@gmail.com>
2024-02-21 23:58:17 +0300

    A

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React-6
+          Learn React-7
         </a>
       </header>
     </div>
